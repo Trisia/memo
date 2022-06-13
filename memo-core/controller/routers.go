2310a11e1efe2da449d8ef15370993d56ec2f500
cliven <quanguanyu@qq.com>
2022-06-13 21:43:10 +0800

    实现前后端分离一站式解决

--- memo-core/controller/routers.go
+++ memo-core/controller/routers.go
@@ -1,11 +1,17 @@
 package controller
 
 import (
+	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
+	"memo-core/setting"
+	"mime"
 )
 
 // routeMapping HTTP路由注册
 func routeMapping(r gin.IRouter) {
+	_ = mime.AddExtensionType(".js", "application/javascript")
+	r.Use(static.Serve("/", static.LocalFile(setting.LocWeb, true)))
+	r = r.Group("/api")
 	NewAuthController(r)
 	NewUserController(r)
 	NewDocController(r)
