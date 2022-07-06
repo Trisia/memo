2c2a82605ab2540875f9d2ce324bc709f27e4228
cliven <quanguanyu@qq.com>
2022-07-06 22:48:56 +0800

    实现了milkdown的图标引用

---
+++ memo-ui/src/components/Milkdown.vue
@@ -0,0 +1,90 @@
+<template>
+    <VueEditor :editor="editor" />
+</template>
+
+<script setup>
+import { Editor, rootCtx, defaultValueCtx, ThemeIcon } from "@milkdown/core";
+import { nord } from "@milkdown/theme-nord";
+import { VueEditor, useEditor } from "@milkdown/vue";
+import { commonmark } from "@milkdown/preset-commonmark";
+import { emoji } from "@milkdown/plugin-emoji";
+import { history } from '@milkdown/plugin-history';
+import { tooltip } from '@milkdown/plugin-tooltip';
+import { menu, menuPlugin, defaultConfig } from "@milkdown/plugin-menu";
+// import { getIcon } from "./icon.js";
+
+
+// const myMenu = menu.configure(menuPlugin, {
+//     config: defaultConfig.map((section) => {
+//         return section.map((item) => {
+//             if (item.type !== 'select') return item;
+
+//             switch (item.text) {
+//                 case 'Heading': {
+//                     return {
+//                         ...item,
+//                         text: '<heading selection>',
+//                         options: [
+//                             { id: '1', text: '<text for h1>' },
+//                             { id: '2', text: '<text for h2>' },
+//                             { id: '3', text: '<text for h3>' },
+//                             { id: '0', text: '<text for paragraph>' },
+//                         ],
+//                     };
+//                 }
+//                 default:
+//                     return item;
+//             }
+//         });
+//     }),
+// });
+
+// const theme = nord.override((emotion, manager) => {
+//     manager.set(ThemeIcon, (icon) => {
+//         if (!icon) return;
+
+//         return getIcon(icon);
+//     });
+// });
+
+const { editor } = useEditor((root) =>
+    Editor.make()
+        .config((ctx) => {
+            ctx.set(rootCtx, root);
+            ctx.set(defaultValueCtx, "# Milkdown 💖 Vue");
+        })
+        .use(nord)
+        .use(emoji)
+        .use(commonmark)
+        .use(history)
+        .use(menu)
+        .use(tooltip)
+);
+
+</script>
+<style>
+
+/* fallback */
+@font-face {
+  font-family: 'Material Icons Outlined';
+  font-style: normal;
+  font-weight: 400;
+  src: url(./gok-H7zzDkdnRel8-DQ6KAXJ69wP1tGnf4ZGhUce.woff2) format('woff2');
+}
+
+.material-icons-outlined {
+  font-family: 'Material Icons Outlined';
+  font-weight: normal;
+  font-style: normal;
+  font-size: 24px;
+  line-height: 1;
+  letter-spacing: normal;
+  text-transform: none;
+  display: inline-block;
+  white-space: nowrap;
+  word-wrap: normal;
+  direction: ltr;
+  -webkit-font-feature-settings: 'liga';
+  -webkit-font-smoothing: antialiased;
+}
+</style>
--- memo-ui/src/view/Test.vue
+++ memo-ui/src/view/Test.vue
@@ -1,67 +1,7 @@
 <template>
-  <split-pane :min="600" style="height: 100%;">
-    <template v-slot:left>
-      <div class="infinite-list-wrapper" style="overflow: auto">
-        <ul v-infinite-scroll="load" class="list" :infinite-scroll-disabled="disabled">
-          <li v-for="i in docArr" :key="i" class="list-item">{{ i }}</li>
-          <p v-if="loading">Loading...</p>
-          <p v-if="noMore">No more</p>
-        </ul>
-      </div>
-    </template>
-    <template v-slot:right>
-      Right
-    </template>
-  </split-pane>
-
+  <Milkdown />
 </template>
-
 <script setup>
-import SplitPane from '../components/SplitPane.vue';
-import axios from 'axios';
-import { computed, ref } from 'vue'
+import Milkdown from "../components/Milkdown.vue"
 
-const docArr = ref([]);
-const loading = ref(false)
-const noMore = computed(() => docArr.value.length >= 20)
-const disabled = computed(() => loading.value || noMore.value)
-const load = () => {
-  loading.value = true;
-  axios.get("./api/doc/search?offset=0&limit=10").then(({ data }) => {
-    console.log(data);
-    for (let i = 0; i < data.length; i++) {
-      docArr.value.push(data[i]);
-    }
-  }).catch((err) => {
-    console.log(err);
-  }).finally(() => {
-    loading.value = false
-  });
-}
 </script>
-
-<style scoped>
-.infinite-list-wrapper {
-  height: 700px;
-  text-align: center;
-}
-
-.infinite-list-wrapper .list {
-  padding: 0;
-  margin: 0;
-  list-style: none;
-}
-
-.infinite-list-wrapper .list-item {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 50px;
-  background: var(--el-color-danger-light-9);
-  color: var(--el-color-danger);
-}
-
-.infinite-list-wrapper .list-item+.list-item {
-  margin-top: 10px;
-}
-</style>
